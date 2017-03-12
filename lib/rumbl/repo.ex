18e9b7a2f0db1b0e2ab863254345899553a04f2e
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-03-12 12:33:05 +0530

    Initial commit
     - Create user model and populate with data
     - Index and show pages for users

---
+++ lib/rumbl/repo.ex
@@ -0,0 +1,22 @@
+defmodule Rumbl.Repo do
+  @moduledoc """
+  In memory repository
+  """
+
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Yedhu", username: "yedhukrishnan", password: "elixir"},
+      %Rumbl.User{id: "2", name: "Crypto", username: "cryptohacker", password: "7langs"},
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do Enum.find all(module), fn map ->
+    Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end) end
+  end
+end
