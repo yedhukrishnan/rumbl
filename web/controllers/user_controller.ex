1f5740159e0950725f032cfde42d6903c7795202
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-03-15 13:45:32 +0530

    Allow users to create accounts

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,6 +1,22 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
 
+  alias Rumbl.User
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    {:ok, user} = Repo.insert(changeset)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: user_path(conn, :index))
+  end
+
   def index(conn, _params) do
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
