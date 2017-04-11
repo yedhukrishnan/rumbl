26c94ea721f382c4259ac5ae7a7ca194e0e11bf4
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-04-11 23:51:24 +0530

    Authenticate users [partial]

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,7 @@
+defmodule SessionController do
+  use Rumbl.Web, :controller
+
+  def new(conn, _) do
+    render conn, "new.html"
+  end
+end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Rumbl.UserController do
   use Rumbl.Web, :controller
+  plug :authenticate when action in [:index, :show]
 
   alias Rumbl.User
 
@@ -14,6 +15,7 @@ defmodule Rumbl.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
@@ -29,5 +31,16 @@ defmodule Rumbl.UserController do
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
   end
 end
